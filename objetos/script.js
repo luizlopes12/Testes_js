480a29be62059bc809ab2939a02159cf4c2c24e4
Luiz Lopes <luizlopes12@outlook.com>
2021-05-17 12:43:20 -0300

    Update script.js

--- objetos/script.js
+++ objetos/script.js
@@ -1,6 +1,12 @@
 function animal() {
     this.nome = "tartaruga";
     this.cor = "verde";
+    this.peso = 5;
+
+    this.comer = function(kg){
+        console.log("comeu");
+        this.peso += (kg/2);
+    }
 }
 var animal1 = new animal();
 var animal2 = new animal();
