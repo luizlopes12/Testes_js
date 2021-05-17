d7ee5f21c04a142820868d59b39a0a329e2291ef
Luiz Lopes <luizlopes12@outlook.com>
2021-05-17 10:18:22 -0300

    defiinindo objetos e propriedades

---
+++ objetos/script.js
@@ -0,0 +1,8 @@
+function animal() {
+    this.nome = "tartaruga";
+    this.cor = "verde";
+}
+var animal1 = new animal();
+var animal2 = new animal();
+animal2.cor = "Branco";
+animal2.nome = "lobo";
