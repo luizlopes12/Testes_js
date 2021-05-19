2566bb991a16f579e187d6bcd7549953efd990fa
Luiz Lopes <luizlopes12@outlook.com>
2021-05-19 15:17:20 -0300

    parametros padroes

---
+++ parametros padroes/script.js
@@ -0,0 +1,10 @@
+//caso não for inserido nenhum dado os campos vão estar com seus valores padroes
+function acao(nome = '', sobrenome = '', status = false){
+
+    //escrevendo os parametros com object literals
+    console.log({nome, sobrenome, status})
+
+
+
+}
+
