c007a9396b8a1a84668cee22305ec7b0d6f0ce2e
Luiz Lopes <luizlopes12@outlook.com>
2021-07-16 19:58:04 -0300

    sla

--- jogoMobile/script.js
+++ jogoMobile/script.js
@@ -5,8 +5,8 @@ function addBola(){
     var bola = document.createElement("div");
     bola.setAttribute("class","bola");
     //math floor transforma um numero decimal em inteiro arredondando pra baixo
-    var p1 =  Math.floor(Math.random() * 1300);
-    var p2 =  Math.floor(Math.random() * 600);
+    var p1 =  Math.floor(Math.random() * 300);
+    var p2 =  Math.floor(Math.random() * 500);
     bola.setAttribute("style", `left: ${p1}px; top: ${p2}px;`);
     bola.setAttribute("onclick", "estourar(this), contador()");;
     document.body.appendChild(bola);
